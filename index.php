cd7986626dae904ff9cbc88a2fb093dd54b2f1d9
Scott Wells <efwells101@gmail.com>
2019-01-14 14:30:47 -0700

    create index.php and basic html structure

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>A Few Facts About Scott</title>
+	</head>
+	<body>
+		<h1>A Few Facts About Scott</h1>
+		<p>Scott was raised in the wilds of central New Mexico.</p>
+		<p>As you may know, indigenous humans from higher elevations are naturally omnivores.
+			This is due to both temperature and elevation.</p>
+		<p>Scott enjoys a variety of foods, including:</p>
+		<ul>
+			<li>Cheeseburgers</li>
+			<li>M&Ms</li>
+			<li>Chocolate Pie</li>
+			<li>Vietmanese Noodle Bowls</li>
+		</ul>
+		<p>This is what he looks like:</p>
+		<img src="scott-avatar.png" alt="scott avatar"/>
+		<p>If you have any more questions about his origins or chemical makeup, please don't hesitate to ask!</p>
+	</body>
+</html>
